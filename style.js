f246a7f4f7c2b8df12a2ba3280021480acd009c9
liralucas <48302150+liralucas@users.noreply.github.com>
2020-04-21 21:47:12 -0300

    Refatoração contatos listados com cards, cores e medidas.

--- style.js
+++ style.js
@@ -1,4 +1,6 @@
 import {StyleSheet} from 'react-native';
+import Cores from './Cores/colors';
+import Medidas from './Medidas/measures';
 
 export default StyleSheet.create({
   container : {
@@ -6,6 +8,9 @@ export default StyleSheet.create({
         paddingHorizontal: 24,
         marginTop: 40,
         backgroundColor: "rgb(255, 68, 68)",
+        paddingHorizontal: Medidas.containerPaddingH,
+        marginTop: Medidas.containerMarginT,
+        backgroundColor: Cores.accent,
   },
   contatoView : {
     padding: 24,
@@ -13,6 +18,10 @@ export default StyleSheet.create({
     backgroundColor: '#FFF',
     marginBottom: 16,
     marginTop: 2,
+    padding: Medidas.cardPadding,
+    borderRadius: Medidas.cardBorderRadius,
+    backgroundColor: Cores.primary,
+    marginVertical: Medidas.cardMargin,
     flexDirection: "column",
     alignItems: "center",
     justifyContent: "center",
@@ -25,10 +34,19 @@ export default StyleSheet.create({
     borderWidth: 1,
     borderRadius: 5,
     color: '#999',
+    width: Medidas.inputWidth,
+    height: Medidas.inputHeight,
+    backgroundColor: Cores.primary,
+    borderColor: Cores.border,
+    borderWidth: Medidas.inputBorderWidth,
+    borderRadius: Medidas.inputBorderRadius,
+    color: Cores.border,
     //borderBottomColor: 'black',
     //borderBottomWidth: 1,
     marginBottom: 10,
     padding: 10
+    marginBottom: Medidas.inputMargin,
+    padding: Medidas.inputPadding,
 
   },
   itemNaLista: {
@@ -36,10 +54,24 @@ export default StyleSheet.create({
     borderRadius: 8,
     backgroundColor: '#FFF',
     marginBottom: 16
+    padding: Medidas.cardPadding,
+    marginBottom: Medidas.cardMargin,
+    borderRadius: Medidas.cardBorderRadius,
+    backgroundColor: Cores.primary,
+    shadowColor: Cores.shadow,
+    shadowOffset: {
+      width: Medidas.cardShadowWidth,
+      height: Medidas.cardShadowHeight
+    },
+    shadowRadius: Medidas.cardShadowRadius,
+    shadowOpacity: Medidas.cardShadowOpacity,
+    elevation: Medidas.cardElevation,
   },
   listaTitle: {
       fontSize: 14,
       color: '#41414D',
+      fontSize: Medidas.fontText,
+      color: Cores.textPrimary,
       fontWeight: 'bold',
   },
 
@@ -47,6 +79,9 @@ export default StyleSheet.create({
       marginTop: 8,
       fontSize: 15,
       color: '#737380',
+      marginTop: Medidas.cardTextMargin,
+      fontSize: Medidas.fontText,
+      color: Cores.textSecondary,
   },
   datailsButton: {
     borderRadius: 8,
@@ -54,24 +89,34 @@ export default StyleSheet.create({
     paddingLeft: 20,
     paddingRight: 20,
     backgroundColor: "rgb(255, 68, 68)",
+    borderRadius: Medidas.buttonBorderRadius,
+    padding: Medidas.buttonPaddingPrimary,
+    paddingLeft: Medidas.buttonPaddingSecondary,
+    paddingRight: Medidas.buttonPaddingSecondary,
+    backgroundColor: Cores.accent,
     alignItems: 'center'
   },
   detailsButtonText: {
     color:'#FFF',
+    color: Cores.primary,
     fontWeight: 'bold',
     fontSize: 18,
+    fontSize: Medidas.fontButton,
   },
   welcome: {
     justifyContent: 'center',
     alignItems: 'center',
     marginTop: 100,
+    marginTop: Medidas.titleMarginT,
   },
   welcomeTitle: {
     marginBottom: 10,
     fontSize: 34,
+    fontSize: Medidas.fontTitle,
     textAlign: 'center',
     fontWeight: 'bold',
     color: '#F5F5F8',
+    color: Cores.primary,
   },
 
 });
