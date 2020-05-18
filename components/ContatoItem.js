92ebe4445b9140b9857484ae8bb2c8c3c9e3a1b9
liralucas <48302150+liralucas@users.noreply.github.com>
2020-05-17 21:06:42 -0300

    mecanismo de navegação
    mecanismo de navegação

--- components/ContatoItem.js
+++ components/ContatoItem.js
@@ -1,17 +1,40 @@
 import React from 'react';
-import {View, TouchableOpacity, Text} from 'react-native';
+import {View, TouchableOpacity, Text, Alert} from 'react-native';
+import Cartao from './Cartao'
 import styles from '../style';
 
 const ContatoItem = (props) => {
+  const deleteAlert = () => Alert.alert(
+    "Excluir Contato",
+    "Você deseja realmente excluir este contato?",
+    [
+      {
+        text: 'Não',
+        style: 'cancel'
+      },
+      {
+        text: 'Sim',
+        onPress: props.onDelete.bind(this, props.chave),
+      }
+    ],
+    {cancelable: false}
+
+  );
+
   return (
+    <Cartao estilos={styles.itemNaLista}>
     <TouchableOpacity 
-      onLongPress={props.onDelete.bind(this, props.chave)}
-    >
-      <View style={styles.itemNaLista}>
-        <Text style={styles.listaTitle}>{"Nome: " + props.contato.nome}</Text>
-        <Text style={styles.listaPhone}>{"Telefone: " + props.contato.telefone}</Text>
-      </View>
-    </TouchableOpacity>
+      onLongPress={deleteAlert}
+      style={{width: '100%'}}
+      onPress={() => props.onShowPageDetail(props.chave, props.contato.nome, 
+        props.contato.telefone)}>
+        <View>
+          <Text style={styles.listaTitle}>{"Nome: " + props.contato.nome}</Text>
+          <Text style={styles.listaPhone}>{"Telefone: " + props.contato.telefone}</Text>
+        </View>
+      </TouchableOpacity>
+    </Cartao>
+    
   );
 }
 
