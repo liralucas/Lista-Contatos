22c420e87d2748a34e3995827d4ba59de50f4ebe
liralucas <48302150+liralucas@users.noreply.github.com>
2020-05-25 21:58:52 -0300

    usando Redux
    usando Redux

---
+++ Contato.js
@@ -0,0 +1,10 @@
+class Contato{
+  constructor(id, nome, telefone, imagemURL){
+  this.id = id;
+  this.nome = nome;
+  this.telefone = telefone;
+  this.imagemURL = imagemURL;
+  }
+}
+
+export default Contato;
