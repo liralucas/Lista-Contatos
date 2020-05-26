08a54c29c7057f96117f8428b0c1d9f6f57d295a
liralucas <48302150+liralucas@users.noreply.github.com>
2020-05-25 21:40:59 -0300

    usando Redux
    usando Redux

--- components/ContatoInput.js
+++ components/ContatoInput.js
@@ -1,11 +1,15 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import {TextInput, TouchableOpacity, Text} from 'react-native';
 import Cartao from './Cartao'
 import styles from '../style';
+import tirarFoto from './TirarFoto';
 
 const ContatoInput = (props) => {
   const[nome, setNome] = useState ("");
   const[telefone, setTelefone] = useState ("");
+  const[imagemURL, setImagemURL] = useState();
+  
+  const fotoRef= useRef(null);
 
   const capturarNome = (textoDigitado) => {
     setNome(textoDigitado)
@@ -15,30 +19,36 @@ const ContatoInput = (props) => {
     setTelefone(textoDigitado)
   }
   const  handleAdicionarContatos  =  ( )  =>  {
-    props.onAdicionarContato (nome,  telefone) ;
+    props.onAdicionarContato (nome,  telefone, ImagemURL) ;
     setNome (" ") ;
     setTelefone (" ") ;
+    fotoRef.current.resetImage();
   }
   return (
     <Cartao estilos={styles.contatoView}>
-       { /*Usuário irá inserir lembretes aqui */}
+        
         <TextInput 
           placeholder="Nome..."
           style={styles.contatoTextInput}
           onChangeText={capturarNome}
-          value={nome}
-        />
+          value={nome} />
+        
         <TextInput 
           placeholder="Telefone..."
           style={styles.contatoTextInput}
           onChangeText={capturarTelefone}
-          value={telefone}
-        />
+          value={telefone} />
+    
+       <TirarFoto
+          onFotoTirada={fotoTirada}
+          imagemURL={imagemURL}
+          ref={fotoRef} />
+    
         <TouchableOpacity
           style={styles.datailsButton}
-          onPress={() => {props.onAdicionarContato(nome, telefone)}}
-        >
-          <Text style={styles.detailsButtonText}>Cadastrar</Text>
+          onPress={() => {props.onAdicionarContato(nome, telefone)}} >
+          
+        <Text style={styles.detailsButtonText}>Cadastrar</Text>
         </TouchableOpacity>
     </Cartao>
   );
